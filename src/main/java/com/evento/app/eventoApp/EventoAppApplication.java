a921ac36fcb4781f22c200833d50ef7bf745ba6c
Rui Matos <rui.microk@gmail.com>
2021-10-02 20:59:18 -0300

    Criação Controller e Model

---
+++ src/main/java/com/evento/app/eventoApp/EventoAppApplication.java
@@ -0,0 +1,13 @@
+package com.evento.app.eventoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evento/app/eventoApp/EventoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evento.app.eventoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
